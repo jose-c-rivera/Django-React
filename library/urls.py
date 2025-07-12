2ab6b960240ac7f99e169c5eb9d7c05e63687475
Jose Rivera <jose.rivera.main@gmail.com>
2025-07-11 21:04:15 -0400

    Split up views and urls to be handled per model

--- library/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ConsoleListCreate, ConsoleVariationListCreate, VariationsByConsole
-
-urlpatterns = [
-    path('api/consoles/', ConsoleListCreate.as_view(), name='console-list-create'),
-    path('api/consoles/variations/', ConsoleVariationListCreate.as_view(), name='console-variation-list'),
-    path('api/consoles/<int:console_id>/variations/', VariationsByConsole.as_view(), name='console-variations')
-]
